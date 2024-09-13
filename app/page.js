bd6c5cfa94c721c884a9ae9be36b4e5e91e3ca55
Tristin Bates <Tristin.Bates@edu.sait.ca>
2024-09-13 13:55:29 -0600

    Completed Week 2 Assignment
    Added the information to the Week 2 assignment page, created the home page and added styling to the home page.

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
+import Link from "next/link";
 
 
 export default function Home() {
   return (
     <main>
-
+      <h1 className="text-4xl font-bold mb-5">CPRG 306: Web Development 2 - Assignments</h1>
+      <Link href="/week-2" className="hover:text-pink-300 hover:underline">Week 2 Assignment</Link>
     </main>
   );
 }
---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+    return (
+        <main>
+            <p>Tristin Bates</p>
+            <Link href="https://github.com/tristinsait/cprg306-assignments">https://github.com/</Link>
+        </main>
+    )
+}
